5022ef0e8ed7a249c43621335417408aba5c7ce7
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2021-03-28 16:18:14 +0200

    first commit

--- frontend/src/components/Wait/index.js
+++ frontend/src/components/Wait/index.js
@@ -6,7 +6,7 @@ import img from 'src/assets/svg/stars.svg';
 const Wait = () => (
   <div className="construction">
     <h1 className="construction__title">Anne-Sophie & Gabriel</h1>
-    <h1>Site en construction</h1>
+    <h1>Site en construction </h1>
     <video controls autoPlay controls loop className="construction__video">
       <source src={video} type="video/mp4"  ></source>
     </video>
