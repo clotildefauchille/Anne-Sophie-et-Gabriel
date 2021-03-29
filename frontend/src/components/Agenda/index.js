11dabc73883ab95f0950f6a2c76800ec2cd343e5
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2021-03-29 23:03:06 +0200

    agenda page scss and responsive architecture

--- frontend/src/components/Agenda/index.js
+++ frontend/src/components/Agenda/index.js
@@ -1,15 +1,102 @@
 import React from 'react';
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
+import img from 'src/assets/svg/agenda.svg';
+import separation from 'src/assets/svg/separation.svg';
+import coktail from 'src/assets/svg/coktail.svg';
+import bell from 'src/assets/svg/bell.svg';
+import brunch from 'src/assets/svg/brunch.svg';
+import cityHall from 'src/assets/svg/cityHall.svg';
+import diner from 'src/assets/svg/diner.svg';
+
 import './style.scss';
 
 const Agenda = () => (
-  <>
-  <Header />
-  <div>salutjksdhdhSJLqhdjlhDJKLqhdjkqlhDJKLSQHJDKLHJQKLHDJKMD lorem jskhdjkqlhsjdhjkgJHGHJGSHJKgjhdqgsjhkGDHJKgsdhjGDHSJgdjlGDS
-  sjkqlDHSJKQMhdjskqmhDJSKQmhdjqskmhdjsqkmhskj</div>
-  <Footer />
-  </>
+  <div className="page">
+    <Header />
+    <div className="agenda">
+      <h2 className="agenda__title">Agenda</h2>
+      <div className="agenda__container">
+        
+        <div className="agenda__container-multiple-event">
+          <h3 className="agenda__day-title">Samedi 11</h3>
+          <div className="agenda__container-event">
+            <img className="agenda__event-icon" alt="cityHall" src={cityHall} />
+            <div className="agenda__container-event-text">
+              <h4 className="agenda__event-title">Voeux à la Mairie, 14h00</h4>
+              <p className="agenda__event-paraph">
+                Rendez-vous à mairie de Thionville <br /> 2 Cours du Château,{' '}
+                <br /> 57100 Thionville, France
+              </p>
+            </div>
+          </div>
+
+          <div className="agenda__container-event">
+            <img className="agenda__event-icon" alt="bell" src={bell} />
+            <div className="agenda__container-event-text">
+              <h4 className="agenda__event-title">
+                Cérémonie religieuse, 15h00
+              </h4>
+              <p className="agenda__event-paraph">
+                Rendez-vous à l'église Saint-Maximim <br /> 3 Place de l’Eglise{' '}
+                <br /> 57100 Thionville, France
+              </p>
+            </div>
+          </div>
+
+          <div className="agenda__container-event">
+            {/* <img src={separation} /> */}
+            <img className="agenda__event-icon" alt="coktail" src={coktail} />
+            <div className="agenda__container-event-text">
+              <h4 className="agenda__event-title">Vin d'honneur, 17H00</h4>
+              <p className="agenda__event-paraph">
+                Rendez-vous à l’abbaye des Prémontrés <br /> 9 rue Saint Martin{' '}
+                <br /> 54700 Pont-à-Moussone, France
+              </p>
+            </div>
+          </div>
+        </div>
+
+      
+          <img
+            className="agenda__img"
+            src={img}
+            alt="anne_sophie & gab picture"
+          />
+      
+
+        <div className="agenda__container-multiple-event">
+
+          <h3 className="agenda__day-title">Samedi Soir</h3>
+          <div className="agenda__container-event">
+            <img className="agenda__event-icon" alt="diner" src={diner} />
+            <div className="agenda__container-event-text">
+              <h4 className="agenda__event-title">Dîner, 20H00</h4>
+              <p className="agenda__event-paraph">
+                Soirée à l’abbaye des Prémontrés <br /> 9 rue Saint Martin{' '}
+                <br /> 54700 Pont-à-Moussone, France
+              </p>
+            </div>
+          </div>
+
+          <h3 className="agenda__day-title">Dimanche 12</h3>
+
+          <div className="agenda__container-event">
+            <img className="agenda__event-icon" alt="brunch" src={brunch} />
+            <div className="agenda__container-event-text">
+              <h4 className="agenda__event-title">Brunch, 11H00</h4>
+              <p className="agenda__event-paraph">
+                Brunch à l’abbaye des Prémontrés <br /> 9 rue Saint Martin{' '}
+                <br /> 54700 Pont-à-Moussone, France
+              </p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
 );
 
 export default Agenda;
