b3edb5ab5f8167a45510a12fe1fc16aff58df6f7
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2021-05-20 10:19:52 +0200

    profil ok on desktop

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -7,37 +7,58 @@ import question from 'src/assets/svg/question.svg';
 import map from 'src/assets/svg/map.svg';
 import response from 'src/assets/svg/response.svg';
 import { NavLink } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+
 // import './style.scss';
 
-const Menu = () => (
-  <div className="header__navbar">
-    <NavLink to="/" exact activeClassName="header__selected">
-      <img className="header__icon" alt="agenda" src={agendaNav} />
-      <img className="header__highlight" src={activeMenu} />
-    </NavLink>
-    <NavLink to="/confirmation" exact activeClassName="header__selected">
-      <img className="header__icon" src={response} alt="confirmation" />
-      <img className="header__highlight" src={activeMenu} />
-    </NavLink>
-    <NavLink to="/infos" exact activeClassName="header__selected">
-      <img className="header__icon" src={map} alt="info pratique" />
-      <img className="header__highlight" src={activeMenu} />
-    </NavLink>
-    <NavLink to="/questions" exact activeClassName="header__selected">
-      <img className="header__icon" src={question} alt="info pratique" />
-      <img className="header__highlight" src={activeMenu} />
-    </NavLink>
-    <NavLink to="/cadeau" exact activeClassName="header__selected">
-      <img className="header__icon" src={gift} alt="cadeaux" />
-      <img className="header__highlight" src={activeMenu} />
-    </NavLink>
-    <button
-    // onClick={handleClickCreateActivity} 
-      type="button" alt="profil" className="header__profile">
-      <img className="header__icon" alt="disconnect" src={profile} />
-      <img className="header__highlight" src={activeMenu} />
-    </button>
-  </div>
-);
+const Menu = () => {
+  const { user, logout } = useAuth0();
+
+  return (
+    <div className="header__navbar">
+      <NavLink to="/" exact activeClassName="header__selected">
+        <img className="header__icon" alt="agenda" src={agendaNav} />
+        <img className="header__highlight" src={activeMenu} />
+      </NavLink>
+      <NavLink to="/confirmation" exact activeClassName="header__selected">
+        <img className="header__icon" src={response} alt="confirmation" />
+        <img className="header__highlight" src={activeMenu} />
+      </NavLink>
+      <NavLink to="/infos" exact activeClassName="header__selected">
+        <img className="header__icon" src={map} alt="info pratique" />
+        <img className="header__highlight" src={activeMenu} />
+      </NavLink>
+      <NavLink to="/questions" exact activeClassName="header__selected">
+        <img className="header__icon" src={question} alt="info pratique" />
+        <img className="header__highlight" src={activeMenu} />
+      </NavLink>
+      <NavLink to="/cadeau" exact activeClassName="header__selected">
+        <img className="header__icon" src={gift} alt="cadeaux" />
+        <img className="header__highlight" src={activeMenu} />
+      </NavLink>
+      <div class="header__dropdown">
+        <button
+          // onClick={handleClickCreateActivity}
+          type="button"
+          alt="profil"
+          className="header__profile-btn"
+        >
+          <img className="header__icon" alt="disconnect" src={profile} />
+          <img className="header__highlight" src={activeMenu} />
+        </button>
+        <div className="header__dropdown-content">
+          <button
+            className="header__logout"
+            type="button"
+            alt="logout Button"
+            onClick={() => logout({ returnTo: window.location.origin })}
+          >
+            Déconnexion
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
