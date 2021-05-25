b78be6e86defeab0874ea74da66d7469b88bf50b
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2021-05-25 14:17:06 +0200

    login css ok

--- frontend/src/components/Profile/index.js
+++ frontend/src/components/Profile/index.js
@@ -6,34 +6,6 @@ const Profile = () => {
   // const [userMetadata, setUserMetadata] = useState(null);
   const [userRoles, setUserRoles] = useState(null);
 
-  // useEffect(() => {
-  //   const getUserMetadata = async () => {
-  //     const domain = "dev-ljslmul5.eu.auth0.com";
-
-  //     try {
-  //       const accessToken = await getAccessTokenSilently({
-  //         audience: `https://${domain}/api/v2/`,
-  //         scope: "read:current_user",
-  //       });
-
-  //       const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-  //       const metadataResponse = await fetch(userDetailsByIdUrl, {
-  //         headers: {
-  //           Authorization: `Bearer ${accessToken}`,
-  //         },
-  //       });
-
-  //       const { user_metadata } = await metadataResponse.json();
-
-  //       setUserMetadata(user_metadata);
-  //     } catch (e) {
-  //       console.log(e.message);
-  //     }
-  //   };
-
-  //   getUserMetadata();
-  // }, []);
 
   useEffect(() => {
     const getUserRoles = async () => {
